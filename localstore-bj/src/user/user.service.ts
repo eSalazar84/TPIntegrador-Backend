865f742c608fefa964e9a79bc0f88e0303e2cd03
Emiliano Salazar <salazaremiliano84@gmail.com>
2023-08-25 15:00:21 -0300

    modified data file

--- localstore-bj/src/user/user.service.ts
+++ localstore-bj/src/user/user.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
+const URL_users = 'http://localhost:3030/users'
 
 @Injectable()
 export class UserService {
+    private async getAll(): Promise<User[]> {
+        try {
+            const res = await fetch(URL_users, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!res.ok) throw new Error('Response not ok');
+            const parsed = res.json();
+            return parsed
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
 
-    async getAllUser()
-
+    async getAllUsers(): Promise<User[]>{
+        try {
+            return this.getAll();
+        } catch (err) {
+            throw new Error(err);            
+        }
+    }
 }
